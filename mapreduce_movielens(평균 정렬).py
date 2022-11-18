a2a37bd6e13dde21e94e690a363594e2d041c6fe
Jung Eun <79435352+JJungEEun@users.noreply.github.com>
2022-11-18 16:12:14 +0900

    Add files via upload

---
+++ mapreduce_movielens(평균 정렬).py
@@ -0,0 +1,40 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+from time import localtime
+
+class PopMovie(MRJob):
+	def steps(self):
+		return [
+				MRStep(mapper=self.map_rating_count,
+					combiner=self.combine_rating_count,
+					reducer=self.reduce_rating_count),
+				MRStep(reducer=self.reduce_sort)
+				]
+
+	def map_rating_count(self,_,line):
+		userid, movieid, rating, timestamp = line.split(',')
+		if userid != 'userId':
+			yield ((movieid), (float(rating),1))
+
+	def combine_rating_count(self, keys, values):
+		totalratings, total = 0, 0
+		for value in values:
+			totalratings += value[0]
+			total += value[1]
+		yield ((keys), (totalratings,total))
+
+	def reduce_rating_count(self, keys, values):
+		totalratings, total = 0, 0
+		for value in values:
+			totalratings += value[0]
+			total += value[1]
+		avg = totalratings/total
+		yield (avg, keys)
+	
+	def reduce_sort(self ,avg, keys):
+		for key in keys:
+			yield (key,avg)
+
+if __name__ =='__main__':
+	PopMovie.run()
+
